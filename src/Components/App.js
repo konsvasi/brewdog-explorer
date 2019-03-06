bbd1c9fe0dc44cf783d7066c7d50a9bec9c67f43
Kostas V <konsvass@outlook.com>
2019-03-06 11:20:25 +0100

    added prototype of cards and cardlist that will display the cards

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import MainPanel from './MainPanel';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <MainPanel />
       </div>
     );
   }
